a31a6a7032cd0da0e50bb542e42e8f117c514697
PeterVantomme <67973870+PeterVantomme@users.noreply.github.com>
2020-07-07 22:26:13 +0200

    fresh

--- src/be/vives/BodyParts.java
+++
@@ -1,8 +0,0 @@
-package be.vives;
-
-public enum BodyParts {
-    HEAD("head"), BODY("body"), LEGS("legs"), ARMS("arms"), RHAND("rightHand"), LHAND("leftHand"), FEET("feet");
-
-    String bodypart;
-    BodyParts(String b) {bodypart = b;}
-}
--- src/be/vives/ItemType.java
+++
@@ -1,8 +0,0 @@
-package be.vives;
-
-public enum ItemType {
-    STAFF("staff"), LSWORD("Longsword"), AXE("battle axe"), DAGGER("dagger"), WHIP("whip"), GUN("gun"), WAND("wand");
-
-    String weapontype;
-    ItemType(String b) {weapontype = b;}
-}
